2ebccc7e53a63de0d4b715484e78115401f56319
Ekramy Latief <ekramyl3000@gmail.com>
2022-04-08 15:41:14 +0200

    feat: Adding profile info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Image from './components/Image.js';
-
+import CardInfo from './components/CardInfo.js';
 function App() {
   return (
     <div className="container">
       <div className="card">
         <Image />
+        <CardInfo />
       </div>
     </div>
   );
---
+++ src/components/CardInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function CardInfo() {
+  return (
+    <div className="card-info">
+      <h1>Ekramy Latief</h1>
+      <h2>Full-Stack Deveolper</h2>
+      <h3>myemail@gmail.com</h3>
+      <div className="buttons">
+        <button>Email</button>
+        <button>LinkedIn</button>
+      </div>
+    </div>
+  );
+}
+
+export default CardInfo;
