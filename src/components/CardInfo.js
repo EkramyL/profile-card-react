bcba7c213bf46dd347a2e1dcfd5d8c179c7ac75f
Ekramy Latief <ekramyl3000@gmail.com>
2022-04-08 17:33:07 +0200

    feat: Adding social icons

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import mail from '../images/Mail.png';
+import linkedin from '../images/linkedin.png';
 
 function CardInfo() {
   return (
@@ -7,8 +9,13 @@ function CardInfo() {
       <h2>Full-Stack Deveolper</h2>
       <h3>myemail@gmail.com</h3>
       <div className="buttons">
-        <button>Email</button>
-        <button>LinkedIn</button>
+        <button className="email-btn">
+          <img src={mail} className="mail-icon" /> Email
+        </button>
+        <button className="linkedin-btn">
+          <img src={linkedin} className="linkedin-icon" />
+          LinkedIn
+        </button>
       </div>
     </div>
   );
